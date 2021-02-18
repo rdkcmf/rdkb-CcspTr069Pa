fd1aca4aa2d8ac055ff2104ad2e97fa9e624de62
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-02-18 10:38:33 +0100

    RDKCOM-2708: RDKBDEV-807: fix crash when ConnectionRequestUsername is empty
    Reason for change:
    Setting Device.ManagementServer.ConnectionRequestUsername as empty string and
    running an RPC from ACS was causing TR069 process to crash.
    When we set the Device.ManagementServer.ConnectionRequestUsername to NULL string
    we were receving a deafult value at the parameter, Eg:- 7405992D-XB3-68:02:b8:02:c4:2c
    The dereferencing at ssp_BbhmWebAcmIfGetCredential was causing the issue.
    Test Procedure:
    1. Set the Device.ManagementServer.ConnectionRequestUsername as empty string.
    2. Ran GetRPCMethods and GetParameterValues methods from ACS.
    3. Verified that tr069 process crash is not occuring anymore.
    4. Set the Device.ManagementServer.ConnectionRequestUsername valid string.
    5. Verified that the GetRPC and GetParameterValues methods are passing successfully.
    Risks: None.
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
    
    (cherry picked from commit 2b646a294a9259fe9b3d2b58bffbe8754be9046e)
    Change-Id: I5a320a5ae92db9ffebfda73d8257b6e450a27a6d

--- source-embedded/Ssp/ssp_web_acm.c
+++ source-embedded/Ssp/ssp_web_acm.c
@@ -126,10 +126,23 @@ ssp_BbhmWebAcmIfGetCredential
             (
                 g_pCcspCwmpCpeController->PANameWithPrefix
             );
-    rc = strcmp_s(pConnReqUsername,MAX_CONN_SIZE,(char*)pUserName,&ind);
-    if((!ind) && (rc == EOK))
+
+    /*
+       pConnReqUsername or pUserName (Fixme: which one?) may be NULL when
+       Device.ManagementServer.ConnectionRequestUsername is set to an empty
+       string, so explicit NULL checks are required.
+    */
+    if (pConnReqUsername && pUserName)
     {
-        *ppPassword = (PUCHAR)AnscCloneString(pConnReqPassword);
+        rc = strcmp_s(pConnReqUsername,MAX_CONN_SIZE,(char*)pUserName,&ind);
+        if((!ind) && (rc == EOK))
+        {
+            *ppPassword = (PUCHAR)AnscCloneString(pConnReqPassword);
+        }
+        else
+        {
+            returnStatus = ANSC_STATUS_FAILURE;
+        }
     }
     else
     {
