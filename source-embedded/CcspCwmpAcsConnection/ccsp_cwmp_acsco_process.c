fac28c9f1e351d50022977583f17cc9ca2cada9a
Andre McCurdy <armccurdy@gmail.com>
2022-03-16 21:21:43 -0700

    RDKCOM-3387: RDKBDEV-1324: make intHttpContent variable local
    Reason for change:
    make intHttpContent variable local to CcspCwmpAcscoRequest() instead of global
    Unprotected global variables are not safe in code which may be used
    in a multi-threaded environment. Since there's no clear reason for
    intHttpContent to be a global variable, fix by making it a local
    variable within CcspCwmpAcscoRequest(). Also drop duplicate zeroing
    of the contents of the variable.
    Test Procedure: Sanity.
    Risks: None.
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    (cherry picked from commit 20237df209442126d0a44328282d8f8f9c17fd2b)
    Change-Id: Ifc4e32f94377572b85ac16a1d2bc192620e40c45

--- source-embedded/CcspCwmpAcsConnection/ccsp_cwmp_acsco_process.c
+++ source-embedded/CcspCwmpAcsConnection/ccsp_cwmp_acsco_process.c
@@ -290,8 +290,6 @@ CcspCwmpAcscoConnect
     return:     the status of the operation;
 
 **********************************************************************/
-ANSC_ACS_INTERN_HTTP_CONTENT    intHttpContent  = { 0 };
-
 ANSC_STATUS
 CcspCwmpAcscoRequest
     (
@@ -313,6 +311,7 @@ CcspCwmpAcscoRequest
     PHTTP_CAS_INTERFACE             pHttpCasIf      = NULL;
     PHTTP_REQUEST_URI               pHttpReqInfo    = NULL;
     ANSC_STATUS                     returnStatus    = ANSC_STATUS_SUCCESS;
+    ANSC_ACS_INTERN_HTTP_CONTENT    intHttpContent;
     PANSC_ACS_INTERN_HTTP_CONTENT   pHttpGetReq     = &intHttpContent;
     BOOL                            bApplyTls       = FALSE;
     PCHAR                           pRequestURL     = NULL;
@@ -330,8 +329,6 @@ CcspCwmpAcscoRequest
     {
         return ANSC_STATUS_NOT_READY;
     }
-
-    AnscZeroMemory(pHttpGetReq, sizeof(ANSC_ACS_INTERN_HTTP_CONTENT));
 
     CcspTr069PaTraceDebug(("CcspCwmpAcscoRequest -- AcsUrl = '%s'\n", pMyObject->AcsUrl));
 
